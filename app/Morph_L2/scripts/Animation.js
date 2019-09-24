c5be3de5d395112245c77b132ab781915d5cbfd6
tim.hennessey <Tim.Hennessey@deutschinc.com>
2019-09-24 16:03:21 -0700

    Initial Commit

---
+++ app/Morph_L2/scripts/Animation.js
@@ -0,0 +1,52 @@
+var app = app || {};
+
+
+app.Animation = (function () {
+
+	var animationWindow = document.getElementById('animationWindow');
+
+    var animData = {
+        wrapper: animationWindow,
+        animType: 'svg',
+        loop: false,
+        prerender: true,
+        autoplay: false,
+        path: './scripts/HRB_Morph_L2.json'
+    };
+
+    var anim = bodymovin.loadAnimation(animData);
+
+	// --------------------------------------------------------------------------------------
+	// set default properties
+	function initialize() {
+		// DO NOT EDIT: reveals banner once loaded
+		// TweenMax.set(anim, {force3D: true});
+
+        anim.setSpeed(1);
+	}
+
+	// --------------------------------------------------------------------------------------
+	// Starts the animation
+	function start() {
+		TweenMax.delayedCall(.5, myFunction);
+
+        function myFunction() {
+            anim.play();
+        }
+	}
+
+	// --------------------------------------------------------------------------------------
+	// Stops the animation
+	function stop() {
+		console.log("stopping animation");
+	}
+
+	// --------------------------------------------------------------------------------------
+	// Publicly accessible methods and properties
+	return {
+		initialize:initialize,
+		start:start,
+		stop:stop
+	}
+
+})();
